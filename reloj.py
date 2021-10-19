7d9ff976869df872afa6ab81bcc035b791e08e63
hugo-araya <hugo.araya.carrasco@gmail.com>
2021-10-19 20:27:18 -0300

    19

---
+++ reloj.py
@@ -0,0 +1,14 @@
+
+def proceso(nombre):
+    ent = open(nombre)
+    sal = open('grado_salida.txt', 'w')
+    grado = int(ent.readline())
+    if (grado % 6) == 0:
+        sal.write('Y'+'\n')
+    else:
+        sal.write('N'+'\n')
+    sal.close()
+    ent.close()
+
+if __name__ == '__main__':
+    proceso('grados.txt')
